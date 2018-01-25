573f81f9b90f188f634f28daf19cf42540613419
Anuj Jain <33128728+ianuj03@users.noreply.github.com>
2018-01-25 14:24:30 +0530

    Create ultraSonic_LCD.ino

---
+++ Arduino/ultraSonic_LCD.ino
@@ -0,0 +1,25 @@
+#include<LiquidCrystal.h>
+#define trig 9
+#define echo 8
+LiquidCrystal lcd(2,3,4,5,6,7);
+void setup(){
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  //Serial.begin(9600);
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  lcd.print("Distance:");
+  digitalWrite(trig,LOW);
+}
+void loop(){
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(12);
+  digitalWrite(trig,LOW);
+  long duration=pulseIn(echo,HIGH);
+  long distance=(duration/2)/29.1;
+  //Serial.println(distance);
+  lcd.setCursor(0,1);
+  lcd.print(distance+1);
+  lcd.print(" cm");
+  //delay(500);
+}
